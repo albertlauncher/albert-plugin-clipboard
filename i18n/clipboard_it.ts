1aa4b03a01cd8929158d11e90314eec8e4c79061
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/clipboard_it.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Copy and paste</source>
+        <translation>Copia e incolla</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Copia</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Rimuovi</translation>
+    </message>
+    <message>
+        <source>Save as snippet</source>
+        <translation>Salva come frammento</translation>
+    </message>
+    <message>
+        <source>History limit</source>
+        <translation>Limite della cronologia</translation>
+    </message>
+    <message>
+        <source>Store history</source>
+        <translation>Salva cronologia</translation>
+    </message>
+</context>
+</TS>
